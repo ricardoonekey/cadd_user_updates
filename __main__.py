4ca9c7448bc06355c4aa2c81294c4c490d637258
Ricardo Rios <ricardo.rios@onekey.com.mx>
2024-12-17 12:33:56 -0600

    add: Read user email and check dalton domain before start searching

--- __main__.py
+++ __main__.py
@@ -1,17 +1,34 @@
 import csv
+import json
+import re
 from pathlib import Path
 
 # CSV files of creation and updates
 creation_file = Path("creation.csv")
 updates_file = Path("updates.csv")
 
+print("Leyendo archivo creation.csv y updates.csv...")
+
 if creation_file.is_file() and updates_file.is_file():
+
+    email_regex = r'^[a-zA-Z0-9._%+-]+@dalton\.com\.mx$'
+    while True:
+        email = input("Ingresa el correo del usuario (debe ser del dominio @dalton.com.mx): ")
+        if re.match(email_regex, email):
+            break
+        else:
+            print("Correo inválido. Asegúrate de ingresar un correo válido con el dominio '@dalton.com.mx'")
+
+    print(f"Comenzando búsqueda del usuario {email}...\n")
+
     # Creation of the user
     try:
         with open(creation_file, mode='r', encoding='utf-8') as file:
             reader = csv.reader(file)
+            next(reader)
             for row in reader:
-                print(row)
+                data = json.loads(row[6])
+                print(data)
     except FileNotFoundError:
         print(f"Error: El archivo '{creation_file}' no existe")
     except PermissionError:
