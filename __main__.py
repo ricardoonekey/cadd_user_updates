113e0f0954bf93fe8d044c2998726929c5f58de3
Ricardo Rios <ricardo.rios@onekey.com.mx>
2024-12-18 15:54:13 -0600

    add: Print user updates without previous data

--- __main__.py
+++ __main__.py
@@ -21,12 +21,13 @@ if creation_file.is_file() and updates_file.is_file():
 
     print(f"Comenzando búsqueda del usuario {email}...\n")
 
+    jdata = None
+    found = False
     # Creation of the user
     try:
         # Load queues data fro JSON file
         with open("queues.json", 'r', encoding='utf-8') as json_file:
             jdata = json.load(json_file)
-        found = False
         with open(creation_file, mode='r', encoding='utf-8') as file:
             reader = csv.reader(file)
             next(reader)
@@ -57,6 +58,43 @@ if creation_file.is_file() and updates_file.is_file():
         print(f"Error: El archivo '{creation_file}' no existe")
     except PermissionError:
         print(f"Error: No tienes permisos para leer el archivo '{creation_file}'.")
+    except Exception as e:
+        print(f"Ocurrió un error inesperado: {e}")
+
+    # Updates of the user
+    try:
+        with open(updates_file, mode='r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            print(f"\n\n####### ACTUALIZACIONES DE: {email} #######")
+            for row in reader:
+                new_data = json.loads(row[6])
+                if new_data["loginId"] == email and row[0] != "System":
+                    print(f"-----------------------------------------------------------------------------")
+                    print("INFORMACION ACTUALIZADA:\n")
+                    print(f"Actualizado por: {row[0]}")
+                    print(f"Fecha de actualización: {row[3]}")
+                    if new_data["features"].get("matching"):
+                        # Print queues if exists
+                        if new_data["features"]["matching"].get("queues"):
+                            print(f"Colas Actualizadas:")
+                            for cola in new_data["features"]["matching"]["queues"]:
+                                for queue_data in jdata:
+                                    if queue_data["queueId"] == cola["queueId"]:
+                                        print(f"- {queue_data["name"]}")
+                        else:
+                            print(f"There are not queues registered at creation time")
+                        # Print attributes if exists
+                        if new_data["features"]["matching"].get("attributes"):
+                            print(f"Atributos:")
+                            for attribute in new_data["features"]["matching"]["attributes"]:
+                                print(f"- {attribute}")
+                    print(f"---------------------------------------------------------------------------")
+    except FileNotFoundError:
+        print(f"Error: El archivo '{updates_file}' no existe")
+    except PermissionError:
+        print(f"Error: No tienes permisos para leer el archivo '{updates_file}'.")
     except Exception as e:
         print(f"Ocurrió un error inesperado: {e}")
 else:
